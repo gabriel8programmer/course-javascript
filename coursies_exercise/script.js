c385d4511c89dd39695210e90e238d5384f77934
gabriel8programmer <gabriel8github@gmail.com>
2023-04-03 14:53:41 -0300

    finish exercise - added event listener for button add before and add later

--- coursies_exercise/script.js
+++ coursies_exercise/script.js
@@ -4,8 +4,10 @@ const course_added = document.getElementById("course");
 const course_selected = document.getElementById("course-selected");
 
 //btns
-//add course
-const btn_add = document.getElementById("btn-add");
+//add course before of the selected course
+const btn_add_before = document.getElementById("btn-add-before");
+//add course after of the selected course
+const btn_add_later = document.getElementById("btn-add-later");
 //remove course
 const btn_remove = document.getElementById("btn-remove");
 //select course
@@ -17,30 +19,9 @@ const coursies_element = document.getElementById("coursies");
 //coursies array
 const coursies = ["HTML", "CSS", "Javascript", "PHP", "React"];
 
-//add cousies in element #course_element
-coursies.map( (course, index) => {
-    
-    //create a new container for course
-    const container_course = document.createElement("div");
-    container_course.setAttribute("class", "course");
-    container_course.setAttribute("id", `course-${index+1}`);
- 
-    //create a legend for the course
-    const legend_course = document.createElement("label");
-    legend_course.innerHTML = course;
+//auxiliar functions
 
-    //create a control for to select a course
-    const control_course = document.createElement("input");
-    control_course.setAttribute("type", "radio");
-    control_course.setAttribute("name", "rdb-course");
-    
-    //add elements (course, legend and control)
-    coursies_element.appendChild(container_course);
-    container_course.appendChild(legend_course);
-    container_course.appendChild(control_course);
-});
-
-//this method return the course selected
+//this function return the course selected
 const get_selected_course = ()=>{
     
     //get all elements course
@@ -60,6 +41,38 @@ const get_selected_course = ()=>{
     
 }
 
+//this function create  new element for a course
+const create_new_course = (name)=>{
+    
+    //create a new container for course
+    const container_course = document.createElement("div");
+    container_course.setAttribute("class", "course");
+ 
+    //create a legend for the course
+    const legend_course = document.createElement("label");
+    legend_course.innerHTML = name;
+
+    //create a control for to select a course
+    const control_course = document.createElement("input");
+    control_course.setAttribute("type", "radio");
+    control_course.setAttribute("name", "rdb-course");
+    
+    //add elements (legend and control) and return the final element of course
+    container_course.appendChild(legend_course);
+    container_course.appendChild(control_course);
+    return container_course;
+}
+
+//add cousies in element #course_element
+coursies.map( (course, index) => {
+    
+    //new element for course
+    const new_course = create_new_course(course);
+    //add course in coursies elements
+    coursies_element.appendChild(new_course);
+
+});
+
 //select course
 btn_select.addEventListener("click", (e)=>{
     
@@ -75,7 +88,68 @@ btn_remove.addEventListener("click", (e)=>{
     
     //const what attribute the value of course selected
     const course = get_selected_course();
-    //remove the course
-    course.remove();
-    course_selected.value = "";
+ 
+    //test if course selected is null or not
+    if (course_selected.value){
+        //remove the course
+        course.remove();
+        course_selected.value = "";
+    }
+    else {
+       course_selected.value = "selecione algum curso";
+    }
+
 });
+
+//add course before
+btn_add_before.addEventListener("click", (e)=>{
+    //const what attribute the value of course selected
+    const course = get_selected_course();
+    
+    //test if course is null or not
+    if (course_added.value){
+        
+        //test if course selected is null or not
+        if (course_selected.value){
+            
+            //create and add course
+            const new_course = create_new_course(course_added.value);
+            coursies_element.insertBefore(new_course, course);
+            course_added.value = "";
+            course_selected.value = "";
+        }
+        else {
+            course_selected.value = "selecione algum curso";
+        }
+    }
+    else {
+        alert("Digite algum curso!");
+    }
+})
+
+//add course later
+btn_add_later.addEventListener("click", (e)=>{
+    //const what attribute the value of course selected
+    const course = get_selected_course();
+
+    //test if course is null or not
+    if (course_added.value){
+        
+        //test if course selected is null or not
+        if (course_selected.value){
+            
+            //create and add course
+            const new_course = create_new_course(course_added.value);
+            coursies_element.insertBefore(new_course, course.nextSibling);
+            course_added.value = "";
+            course_selected.value = "";
+        }
+        else {
+            course_selected.value = "selecione algum curso";
+        }
+    }
+    else {
+        alert("Digite algum curso!");
+    }
+})
+

d60862fb8764a34d30ac606e05ee7b7888133cbe
gabriel8programmer <gabriel8github@gmail.com>
2023-04-03 00:04:10 -0300

    added class 43 - exercise with add, remove and filter elements

---
+++ class043_exercise/script.js
@@ -0,0 +1,14 @@
+
+//select form
+//coursies form
+const select_course = document.getElementById("course");
+const coursies = [...document.querySelectorAll(".course")];
+
+//btns
+//add course
+const btn_add = document.getElementById("btn-add");
+//remove course
+const btn_remove = document.getElementById("btn-remove");
+//select course
+const btn_select = document.getElementById("btn-select");
+
